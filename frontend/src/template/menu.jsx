efc935a2a53d85a07f538d49cdc3188085f780e7
Alan Kendy Higa <alankhiga@gmail.com>
2020-02-12 21:58:19 -0300

    adding router

--- frontend/src/template/menu.jsx
+++ frontend/src/template/menu.jsx
@@ -4,7 +4,7 @@ export default props => (
   <nav className='navbar navbar-inverse bg-inverse'>
     <div className='container'>
       <div className='navbar-header'>
-        <a className='navbar-brand'>
+        <a className='navbar-brand' href= '#'>
           <i className='fa fa-calendar-check-o'></i> TodoApp
         </a>
       </div>
