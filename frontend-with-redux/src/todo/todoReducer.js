e39313eae7846e92b38b43d520d4d67eb25ba89e
Alan Kendy Higa <alankhiga@gmail.com>
2020-03-22 18:42:33 -0300

    middleware added;

--- frontend-with-redux/src/todo/todoReducer.js
+++ frontend-with-redux/src/todo/todoReducer.js
@@ -1,19 +1,4 @@
-const INITIAL_STATE = {
-  description: 'Read books',
-  list: [{
-    _id: 1,
-    description: 'Pay bills',
-    done: true
-  }, {
-    _id: 2,
-    description: 'Medical check-up',
-    done: false
-  }, {
-    _id: 3,
-    description: 'Buy groceries',
-    done: false
-  }]
-}
+const INITIAL_STATE = { description: '',  list: [ ] }
 
 export default (state = INITIAL_STATE, action) => {
   switch(action.type) {
