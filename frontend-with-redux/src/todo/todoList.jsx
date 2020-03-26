e7b14bb1f6df67bb35af2b34a88396fc149e5c80
Alan Kendy Higa <alankhiga@gmail.com>
2020-03-25 22:45:09 -0300

    redux added, list wip

--- frontend-with-redux/src/todo/todoList.jsx
+++ frontend-with-redux/src/todo/todoList.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import  { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+
 import IconButton from '../template/iconButton'
+import { markAsDone, markAsPending, remove } from './todoActions'
 
 const TodoList = props => {
 
@@ -14,17 +17,17 @@ const TodoList = props => {
             style='success'
             icon= 'check'
             hide={todo.done}
-            onClick={() => props.handleMarkAsDone(todo)}/>
+            onClick={() => props.markAsDone(todo)}/>
           <IconButton
             style='warning'
             icon='undo'
             hide={!todo.done}
-            onClick={() => props.handleMarkAsPending(todo)}/>
+            onClick={() => props.markAsPending(todo)}/>
           <IconButton
             style='danger'
             icon='trash-o'
             hide={!todo.done}
-            onClick={() => props.handleRemove(todo)}/>
+            onClick={() => props.remove(todo)}/>
         </td>
       </tr>
     )
@@ -46,4 +49,6 @@ const TodoList = props => {
 }
 
 const mapStateToProps = state => ({list: state.todo.list})
-export default connect(mapStateToProps)(TodoList)
+const mapDispatchToProps = dispatch =>
+  bindActionCreators({ markAsDone, markAsPending, remove }, dispatch)
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList)
